8ffc53e968c75701b66cfb986518697930303a24
Zeeion12 <tioaganis96@gmail.com>
2024-07-11 09:12:58 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+// Text Animation
+var typed = new Typed(".autotext", {
+    strings : ["Frontend Developer.", "Web Designer.", "Youtuber" ],
+    typeSpeed : 100,
+    backSpeed : 100,
+    loop : true,
+})
+
+// Animasi navbar ketika di scroll
+document.addEventListener('DOMContentLoaded', function() {
+    var navbar = document.getElementById('Navbar0');
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+});
+
+// Animasi Fade-in
+document.addEventListener('DOMContentLoaded', function() {
+    const fadeIns = document.querySelectorAll('.fade-in');
+
+    const observer = new IntersectionObserver(function(entries) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }, { threshold: 0.1 });
+
+    fadeIns.forEach(fadeIn => {
+        observer.observe(fadeIn);
+    });
+});
